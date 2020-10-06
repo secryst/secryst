54fee786064cd7fe75d509cc22f0eb6325912fc4
Ronald Tse <ronald.tse@ribose.com>
2020-10-06 16:59:41 +0800

    Add CLI in GHA, fix bugs

--- lib/secryst.rb
+++ lib/secryst.rb
@@ -9,3 +9,15 @@ require "secryst/vocab"
 require "secryst/transformer"
 
 require "secryst/translator"
+
+module Secryst
+  DEFAULT_HYPERPARAMETERS = {
+    d_model: 64,
+    nhead: 8,
+    num_encoder_layers: 4,
+    num_decoder_layers: 4,
+    dim_feedforward: 256,
+    dropout: 0.05,
+    activation: 'relu',
+  }
+end
