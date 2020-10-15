1aa8e8dd9c009093f28a76f201515add893749cd
Anton Murygin <mibus32@gmail.com>
2020-10-15 21:31:36 +0300

    Add support for ONNX import
    Add test for ONNX import
    
    Fix some things
    
    Add readme
    
    Transformers first
    
    Onnx import test
    
    Fixes
    
    Other way of fetching site dir
    
    Try this
    
    Add [0]
    
    Try like this
    
    Gosh
    
    Like this
    
    Fix this

--- lib/secryst/translator.rb
+++ lib/secryst/translator.rb
@@ -1,46 +1,27 @@
 module Secryst
   class Translator
+    attr_accessor :model, :input_vocab, :target_vocab
     def initialize(model_file:)
       model_name, model, metadata, model_state_dict, vocabs = nil
       @device = "cpu"
       # Unzip model in memory
       Zip::File.open(model_file) do |zip_file|
         metadata = zip_file.glob('metadata.yaml').first
-        raise 'metadata.yaml is missing in model zip!' if !metadata
-        metadata = YAML.load(metadata.get_input_stream.read)
-
-        model = zip_file.glob('model.pth').first
-        raise 'model.pth is missing in model zip!' if !model
-        model_state_dict = Torch.send :to_ruby, Torch._load(model.get_input_stream.read)
+        metadata = YAML.load(metadata.get_input_stream.read) if metadata
 
         vocabs = zip_file.glob('vocabs.yaml').first
         raise 'vocabs.yaml is missing in model zip!' if !vocabs
         vocabs = YAML.load(vocabs.get_input_stream.read)
+        @input_vocab = Vocab.new(vocabs["input"], specials: [])
+        @target_vocab = Vocab.new(vocabs["target"], specials: [])
+
+        model = zip_file.glob('*.pth').first
+        if !model
+          model = zip_file.glob('*.onnx').first
+          raise 'Both .pth and .onnx model files is missing in model zip!' if !model
+        end
+        @model = Model.from_file(model, metadata, @input_vocab.length, @target_vocab.length)
       end
-
-      model_name = metadata.delete("name")
-      # load vocabs
-      @input_vocab = Vocab.new(vocabs["input"])
-      @target_vocab = Vocab.new(vocabs["target"])
-
-      if model_name == 'transformer'
-        @model = Secryst::Transformer.new({
-          d_model: metadata[:d_model],
-          nhead: metadata[:nhead],
-          num_encoder_layers: metadata[:num_encoder_layers],
-          num_decoder_layers: metadata[:num_decoder_layers],
-          dim_feedforward: metadata[:dim_feedforward],
-          dropout: metadata[:dropout],
-          activation: metadata[:activation],
-          input_vocab_size: @input_vocab.length,
-          target_vocab_size: @target_vocab.length,
-        })
-      else
-        raise ArgumentError, 'Only transformer model is currently supported'
-      end
-
-      @model.load_state_dict(model_state_dict)
-      @model.eval
     end
 
     def translate(phrase, max_seq_length: 100)
