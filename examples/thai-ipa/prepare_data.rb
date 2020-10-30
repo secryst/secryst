0e830f2e8c47999d8d069014e7818a8ba9ae9e31
Anton Murygin <mibus32@gmail.com>
2020-10-30 17:14:37 +0200

    Implement Thai to IPA

---
+++ examples/thai-ipa/prepare_data.rb
@@ -0,0 +1,9 @@
+lines = File.readlines('./data/wiktionary-11-2-2020.tsv')
+FileUtils.rm_rf('./data/input.csv')
+FileUtils.rm_rf('./data/target.csv')
+
+lines.each do |line|
+  input, target = line.strip.split("\t")
+  File.write('./data/input.csv', input + "\n", {mode: 'a'})
+  File.write('./data/target.csv', target + "\n", {mode: 'a'})
+end
